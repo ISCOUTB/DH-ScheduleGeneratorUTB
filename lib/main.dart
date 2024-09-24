767a0e180e694b8d21fe4d595ea5519b62479c26
Diego Peña Páez. <diegopena1914@gmail.com>
2024-09-24 12:00:42 -0500

    Update main.dart: El botón genera un horario

--- lib/main.dart
+++ lib/main.dart
@@ -31,15 +31,14 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int selectedIndex = 0; // Para rastrear el índice seleccionado en el riel
-  List<Map<String, dynamic>> searchResults = []; // Resultados de búsqueda
-  int usedCredits = 0; // Créditos usados
-  final int creditLimit = 20; // Límite de créditos
-  List<Map<String, dynamic>> addedSubjects = []; // Lista de materias agregadas
+  int selectedIndex = 0;
+  List<Map<String, dynamic>> searchResults = [];
+  int usedCredits = 0;
+  final int creditLimit = 20;
+  List<Map<String, dynamic>> addedSubjects = [];
 
   final TextEditingController subjectController = TextEditingController();
 
-  // Lista de posibles nombres de materias
   final List<String> possibleSubjects = [
     'Matemáticas',
     'Física',
@@ -53,7 +52,6 @@ class _MyHomePageState extends State<MyHomePage> {
     'Música'
   ];
 
-  // Lista de posibles días
   final List<String> possibleDays = [
     'Lunes',
     'Martes',
@@ -63,45 +61,36 @@ class _MyHomePageState extends State<MyHomePage> {
     'Sábado'
   ];
 
-  // Mapa para almacenar créditos por nombre de materia
   final Map<String, int> subjectCredits = {};
 
-  // Función para generar una hora aleatoria en formato de 24 horas.
   String getRandomTime() {
     Random random = Random();
-    int hour = random.nextInt(12) + 8; // Horas entre 8:00 y 20:00
-    int minuteStart = 0; // Minuto de inicio siempre es 00
-    int endHour = hour + 2; // Clases de 2 horas
-    int minuteEnd = 50; // Minuto final siempre es 50
+    int hour = random.nextInt(12) + 8;
+    int minuteStart = 0;
+    int endHour = hour + 2;
+    int minuteEnd = 50;
 
     return '${hour.toString().padLeft(2, '0')}:${minuteStart.toString().padLeft(2, '0')} - ${endHour.toString().padLeft(2, '0')}:${minuteEnd.toString().padLeft(2, '0')}';
   }
 
-  // Función para generar una lista de materias aleatorias con créditos
   List<Map<String, dynamic>> generateRandomSubjects(int count) {
     Random random = Random();
     List<Map<String, dynamic>> generatedSubjects = [];
 
     for (int i = 0; i < count; i++) {
-      // Seleccionar un nombre de materia al azar
       String subjectName =
           possibleSubjects[random.nextInt(possibleSubjects.length)];
-
-      // Verificar si la materia ya tiene créditos asignados
       int credits;
       if (subjectCredits.containsKey(subjectName)) {
-        credits = subjectCredits[subjectName]!; // Obtener créditos existentes
+        credits = subjectCredits[subjectName]!;
       } else {
-        // Generar un número aleatorio de créditos entre 2 y 4
         credits = random.nextInt(3) + 2;
-        subjectCredits[subjectName] = credits; // Asignar créditos al mapa
+        subjectCredits[subjectName] = credits;
       }
 
-      // Generar un número aleatorio de días para la materia (2 o 3)
       int numDays = random.nextInt(2) + 2;
       List<Map<String, String>> schedule = [];
 
-      // Seleccionar días aleatorios sin repetir
       List<String> availableDays = List.from(possibleDays);
       for (int j = 0; j < numDays; j++) {
         String day =
@@ -110,11 +99,10 @@ class _MyHomePageState extends State<MyHomePage> {
         schedule.add({'day': day, 'time': time});
       }
 
-      // Agregar la materia con su horario y créditos a la lista generada
       generatedSubjects.add({
         'name': subjectName,
         'schedule': schedule,
-        'credits': credits, // Agregar créditos a la materia
+        'credits': credits,
       });
     }
 
@@ -124,7 +112,6 @@ class _MyHomePageState extends State<MyHomePage> {
   void searchSubject(String subject) {
     String lowerCaseSubject = subject.toLowerCase();
 
-    // Buscar si el nombre ingresado coincide con alguna materia en la lista generada
     setState(() {
       searchResults = subjects
           .where((element) =>
@@ -132,7 +119,6 @@ class _MyHomePageState extends State<MyHomePage> {
           .toList();
 
       if (searchResults.isEmpty) {
-        // Mostrar mensaje si no se encontró la materia
         ScaffoldMessenger.of(context).showSnackBar(
           const SnackBar(content: Text('No se encontró la materia')),
         );
@@ -140,7 +126,6 @@ class _MyHomePageState extends State<MyHomePage> {
     });
   }
 
-  // Función para verificar conflictos de horarios
   bool hasScheduleConflict(List<Map<String, String>> newSchedule) {
     for (var newClass in newSchedule) {
       String newDay = newClass['day']!;
@@ -152,26 +137,24 @@ class _MyHomePageState extends State<MyHomePage> {
         for (var existingClass in existingSchedule) {
           if (existingClass['day'] == newDay &&
               existingClass['time'] == newTime) {
-            return true; // Hay un conflicto
+            return true;
           }
         }
       }
     }
-    return false; // No hay conflictos
+    return false;
   }
 
-  void addCredits(
-      String subjectName, List<Map<String, String>> schedule, int credits) {
-    bool alreadyAdded =
-        addedSubjects.any((subject) => subject['name'] == subjectName);
-
-    if (alreadyAdded) {
+  void addCredits(String subjectName, List<Map<String, String>> schedule, int credits) {
+    // Verificar si la materia ya está agregada
+    if (addedSubjects.any((subject) => subject['name'] == subjectName)) {
       ScaffoldMessenger.of(context).showSnackBar(
-        const SnackBar(content: Text('Esta materia ya está en tu horario')),
+        SnackBar(content: Text('Materia ya agregada: $subjectName')),
       );
       return;
     }
 
+    // Verificar si hay un conflicto de horarios
     if (hasScheduleConflict(schedule)) {
       ScaffoldMessenger.of(context).showSnackBar(
         const SnackBar(content: Text('Conflicto de horarios con otra materia')),
@@ -181,9 +164,14 @@ class _MyHomePageState extends State<MyHomePage> {
 
     setState(() {
       if (usedCredits + credits <= creditLimit) {
-        usedCredits += credits; // Sumar créditos de la materia agregada
+        usedCredits += credits;
         addedSubjects.add(
-            {'name': subjectName, 'schedule': schedule, 'credits': credits});
+          {
+            'name': subjectName,
+            'schedule': schedule,
+            'credits': credits,
+          },
+        );
         ScaffoldMessenger.of(context).showSnackBar(
           SnackBar(content: Text('Materia agregada: $subjectName')),
         );
@@ -195,26 +183,72 @@ class _MyHomePageState extends State<MyHomePage> {
     });
   }
 
+  // Nueva función para generar el horario semanal
+  List<Map<String, List<String>>> generateWeeklySchedule() {
+    List<Map<String, List<String>>> weeklySchedule = [];
+
+    for (var day in possibleDays) {
+      List<String> subjectsForDay = [];
+      for (var subject in addedSubjects) {
+        for (var schedule in subject['schedule']) {
+          if (schedule['day'] == day) {
+            subjectsForDay.add('${subject['name']} (${schedule['time']})');
+          }
+        }
+      }
+      weeklySchedule.add({day: subjectsForDay});
+    }
+    return weeklySchedule;
+  }
+
   void generateSchedule() {
     if (addedSubjects.isEmpty) {
       ScaffoldMessenger.of(context).showSnackBar(
         const SnackBar(content: Text('Seleccione materias antes de generar')),
       );
     } else {
-      // Aquí puedes agregar la lógica para generar horarios
-      ScaffoldMessenger.of(context).showSnackBar(
-        const SnackBar(content: Text('Generando horarios...')),
+      List<Map<String, List<String>>> weeklySchedule = generateWeeklySchedule();
+      // Muestra el horario semanal en un diálogo o en la pantalla
+      showDialog(
+        context: context,
+        builder: (context) {
+          return AlertDialog(
+            title: const Text('Horario Semanal'),
+            content: SingleChildScrollView(
+              child: Column(
+                children: weeklySchedule.map((daySchedule) {
+                  String day = daySchedule.keys.first;
+                  List<String> subjects = daySchedule[day]!;
+                  return Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(day,
+                          style: const TextStyle(fontWeight: FontWeight.bold)),
+                      ...subjects.map((subject) => Text(subject)).toList(),
+                      const SizedBox(height: 10),
+                    ],
+                  );
+                }).toList(),
+              ),
+            ),
+            actions: <Widget>[
+              TextButton(
+                onPressed: () => Navigator.of(context).pop(),
+                child: const Text('Cerrar'),
+              ),
+            ],
+          );
+        },
       );
     }
   }
 
-  // Generar materias aleatorias al iniciar
   List<Map<String, dynamic>> subjects = [];
 
   @override
   void initState() {
     super.initState();
-    subjects = generateRandomSubjects(20); // Generar 20 materias aleatorias
+    subjects = generateRandomSubjects(20);
   }
 
   @override
@@ -246,8 +280,7 @@ class _MyHomePageState extends State<MyHomePage> {
             onDestinationSelected: (int index) {
               setState(() {
                 selectedIndex = index;
-                searchResults =
-                    []; // Resetear los resultados al cambiar de sección
+                searchResults = [];
               });
             },
           ),
@@ -256,7 +289,7 @@ class _MyHomePageState extends State<MyHomePage> {
               child: Column(
                 mainAxisAlignment: MainAxisAlignment.start,
                 children: <Widget>[
-                  const SizedBox(height: 20), // Espacio superior
+                  const SizedBox(height: 20),
                   Padding(
                     padding: const EdgeInsets.symmetric(horizontal: 20),
                     child: Text(
@@ -269,128 +302,79 @@ class _MyHomePageState extends State<MyHomePage> {
                     ),
                   ),
                   const SizedBox(height: 20),
-                  if (selectedIndex == 0) // Solo mostrar en "Inicio"
+                  if (selectedIndex == 0) ...[
+                    // Mostrar créditos usados y límite de créditos en el menú
                     Padding(
                       padding: const EdgeInsets.symmetric(horizontal: 20),
                       child: Column(
                         children: [
-                          TextField(
-                            controller: subjectController,
-                            decoration: const InputDecoration(
-                              labelText: 'Por materia...',
-                              border: OutlineInputBorder(),
-                            ),
-                          ),
-                          const SizedBox(height: 20),
-                          ElevatedButton(
-                            onPressed: () {
-                              searchSubject(subjectController
-                                  .text); // Buscar la materia ingresada
-                            },
-                            child: const Text('Buscar'),
-                          ),
-                          const SizedBox(height: 20),
-                          if (searchResults.isNotEmpty) ...[
-                            // Mostrar los resultados de búsqueda con días y horarios
-                            SizedBox(
-                              height:
-                                  300, // Altura ajustada para la lista de resultados
-                              child: ListView.builder(
-                                itemCount: searchResults.length,
-                                itemBuilder: (context, index) {
-                                  var subject = searchResults[index];
-                                  return Card(
-                                    child: Column(
-                                      children: [
-                                        Text(
-                                          'Materia: ${subject['name']}',
-                                          style: const TextStyle(
-                                              fontWeight: FontWeight.bold),
-                                        ),
-                                        ...subject['schedule']
-                                            .map<Widget>((schedule) {
-                                          return Text(
-                                            '${schedule['day']}: ${schedule['time']}',
-                                            textAlign: TextAlign.center,
-                                          );
-                                        }).toList(),
-                                        Text(
-                                          'Créditos: ${subject['credits']}',
-                                          style: const TextStyle(
-                                              fontWeight: FontWeight.bold),
-                                        ),
-                                        ElevatedButton(
-                                          onPressed: () {
-                                            addCredits(
-                                                subject['name'],
-                                                subject['schedule'],
-                                                subject['credits']);
-                                          },
-                                          child: const Text('Agregar'),
-                                        ),
-                                      ],
-                                    ),
-                                  );
-                                },
-                              ),
-                            ),
-                          ] else
-                            Text(
-                              'Resultados de búsqueda aparecerán aquí.',
-                              style: Theme.of(context).textTheme.titleLarge,
-                            ),
-                          const SizedBox(height: 20),
-                          Text(
-                            'Créditos usados: $usedCredits. Límite de créditos: $creditLimit.',
-                            style: Theme.of(context).textTheme.titleMedium,
-                          ),
+                          Text('Créditos usados: $usedCredits'),
+                          Text('Límite de créditos: $creditLimit'),
                         ],
                       ),
                     ),
-                  if (selectedIndex == 1) // Mostrar las materias agregadas
+                    const SizedBox(height: 20),
+                    // Sección para buscar materias
                     Padding(
                       padding: const EdgeInsets.symmetric(horizontal: 20),
-                      child: Column(
-                        children: [
-                          const SizedBox(height: 20),
-                          if (addedSubjects.isEmpty)
-                            const Text('No hay materias agregadas.')
-                          else
-                            Column(
-                              children: addedSubjects.map((subject) {
-                                return ListTile(
-                                  title: Text(subject['name']!),
-                                  subtitle: Column(
-                                    crossAxisAlignment:
-                                        CrossAxisAlignment.start,
-                                    children: subject['schedule']
-                                        .map<Widget>((schedule) => Text(
-                                              '${schedule['day']}: ${schedule['time']}',
-                                            ))
-                                        .toList(),
-                                  ),
-                                  trailing:
-                                      Text('Créditos: ${subject['credits']}'),
-                                );
-                              }).toList(),
+                      child: TextField(
+                        controller: subjectController,
+                        decoration: const InputDecoration(
+                          labelText: 'Buscar materia',
+                          border: OutlineInputBorder(),
+                        ),
+                      ),
+                    ),
+                    const SizedBox(height: 10),
+                    ElevatedButton(
+                      onPressed: () {
+                        searchSubject(subjectController.text);
+                      },
+                      child: const Text('Buscar'),
+                    ),
+                    const SizedBox(height: 20),
+                    // Mostrar resultados de búsqueda
+                    if (searchResults.isNotEmpty)
+                      ...searchResults.map((subject) {
+                        return Card(
+                          margin: const EdgeInsets.symmetric(vertical: 10),
+                          child: ListTile(
+                            title: Text(subject['name']),
+                            subtitle: Text('Créditos: ${subject['credits']}'),
+                            trailing: ElevatedButton(
+                              onPressed: () {
+                                addCredits(subject['name'], subject['schedule'], subject['credits']);
+                              },
+                              child: const Text('Agregar'),
                             ),
-                        ],
-                      ),
-                    ),
-                  if (selectedIndex ==
-                      2) // Mostrar solo el botón en la sección "Horarios"
-                    Padding(
-                      padding: const EdgeInsets.symmetric(horizontal: 20),
-                      child: Column(
-                        children: [
-                          const SizedBox(height: 20),
-                          ElevatedButton(
-                            onPressed: generateSchedule,
-                            child: const Text('Generar Horarios'),
                           ),
-                        ],
+                        );
+                      }).toList(),
+                  ] else if (selectedIndex == 1) ...[
+                    // Mostrar materias agregadas
+                    if (addedSubjects.isEmpty)
+                      const Text('No se han agregado materias.')
+                    else
+                      ...addedSubjects.map((subject) {
+                        return Card(
+                          margin: const EdgeInsets.symmetric(vertical: 10),
+                          child: ListTile(
+                            title: Text(subject['name']),
+                            subtitle: Text('Créditos: ${subject['credits']}'),
+                          ),
+                        );
+                      }).toList(),
+                  ] else if (selectedIndex == 2) ...[
+                    // Sección de horarios
+                    if (addedSubjects.isEmpty)
+                      const Text('No se han agregado materias.')
+                    else ...[
+                      ElevatedButton(
+                        onPressed: generateSchedule,
+                        child: const Text('Generar Horario'),
                       ),
-                    ),
+                    ],
+                  ],
                 ],
               ),
             ),
@@ -400,4 +384,5 @@ class _MyHomePageState extends State<MyHomePage> {
     );
   }
 }
+
 
