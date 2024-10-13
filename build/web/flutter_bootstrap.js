0164693c7c0c8e6eccfadd19919aa074af30ab4c
Gavimantilla <gmantilla@utb.edu.co>
2024-10-13 00:29:48 -0500

    Enésimo commit por no hacer el flutter build web antes de hacer un commit

--- build/web/flutter_bootstrap.js
+++ build/web/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"a6bd3f1de158bb61090e0c8053df93a10cb548
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1556106271"
+    serviceWorkerVersion: "3596301942"
   }
 });
